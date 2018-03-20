57e33fcafd1b2b7ed36cf8feca8b5068ef77d20f
caasi Huang <caasi.igd@gmail.com>
2018-03-20 13:44:08 +0800

    Cleanup

--- src/tiddly-data.ts
+++ src/tiddly-data.ts
@@ -1,6 +1,3 @@
-import * as P from 'parsimmon';
-import { reduceRight, reduce, mergeDeepRight } from 'ramda';
-
 export type Timestamp = number;
 
 export class Wiki {
@@ -50,38 +47,3 @@ export class TMap extends Wiki {
     this.id = id;
   }
 }
-
-// the Wikitext parser
-export const WikitextParser = P.createLanguage({
-  Colon: () =>
-    P.string(':'),
-  Dot: () =>
-    P.string('.'),
-  Symbol: () =>
-    // XXX: should follow the JSON string definition
-    P.regexp(/[a-z0-9_]+/)
-      .map(r => [r]),
-  Key: (r) =>
-    P.alt(
-      P.seqMap(
-        r.Symbol, r.Dot, r.Key,
-        (xs, _, ys) => xs.concat(ys)
-      ),
-      r.Symbol
-    ),
-  Value: () =>
-    P.regexp(/[^\n]*/),
-  Pair: (r) =>
-    P.seqMap(
-      r.Key, P.optWhitespace, r.Colon, P.optWhitespace, r.Value,
-      (ks: string[], _, __, ___, value) => reduceRight((k, acc) => ({ [k]: acc }), value, ks)
-    ),
-  Header: (r) =>
-    r.Pair.trim(P.optWhitespace).many()
-      .map(reduce(mergeDeepRight, {})),
-  File: (r) =>
-    P.seqMap(
-      r.Header, P.all,
-      (header, content) => ({ header, content })
-    )
-});
