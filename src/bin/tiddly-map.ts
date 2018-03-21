f37fce8bba817111d190d3ced1d88044a8903909
caasi Huang <caasi.igd@gmail.com>
2018-03-21 18:59:59 +0800

    Read and check if every nodes exist

--- src/bin/tiddly-map.ts
+++ src/bin/tiddly-map.ts
@@ -4,6 +4,9 @@ import { readdir, readFile } from 'fs-extra';
 import * as yargs from 'yargs';
 import * as D from '../tiddly-data';
 import * as P from '../parser';
+import { keys } from 'ramda';
+
+const TIDDLY_MAP_PREFIX = '$__plugins_felixhayashi_tiddlymap';
 
 const argv = yargs
   .usage('Usage: $0 <command> [options]')
@@ -11,21 +14,47 @@ const argv = yargs
   .alias('d', 'directory')
   .argv;
 
-const file_map: { [key: string]: D.Wiki } = {};
+// states
+const file_map = {};
+const node_map: { [key: string]: D.Node } = {};
+let default_map
 
 const main = async () => {
   const dir_path = path.resolve(argv.directory);
   const files = await readdir(dir_path);
 
+  // read all tid files from the target directory
   for (const file of files) {
+    if (!/.tid$/.test(file)) continue;
+
     const file_path = path.resolve(dir_path, file);
     const data = await readFile(file_path, 'utf8');
 
     const raw = P.TiddlyFile.File.tryParse(data);
-    file_map[file_path] = new D.Wiki(raw);
+    file_map[file] = raw;
+
+    if (raw.tmap && raw.tmap.id) {
+      node_map[raw.tmap.id] = new D.Node(raw);
+    }
   }
 
-  console.log(file_map);
+  // find the default map
+  const default_map_name = `${TIDDLY_MAP_PREFIX}_graph_views_Default_map.tid`;
+
+  if (!file_map[default_map_name]) {
+    throw new Error('default map not fonud!');
+  }
+
+  default_map = new D.DefaultMap(file_map[default_map_name])
+
+  // check if every tmap exists
+  for (const k in default_map.nodeMap) {
+    if (!node_map[k]) {
+      console.warn(`missing node: ${k}`);
+    }
+  }
+
+  console.log(default_map);
 }
 
 main().catch(console.error.bind(console));
